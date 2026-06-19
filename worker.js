51c4407255846c3712c408540ca73a501034a0ac
shunhang776 <2038342171@qq.com>
2026-06-20 03:09:52 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,15 @@
 export default {
   async fetch(request) {
-    console.log("webhook test 123");
-    return new Response("Worker OK");
+    const body = await request.json();
+
+    const repo = body.repository?.full_name;
+    const commits = body.commits || [];
+
+    return new Response(JSON.stringify({
+      ok: true,
+      repo,
+      commit_count: commits.length,
+      last_commit: commits[commits.length - 1]?.message
+    }));
   }
 };
